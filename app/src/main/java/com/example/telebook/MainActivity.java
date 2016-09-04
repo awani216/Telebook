855b51a54d07d71722bc8520407802916fa64ccf
Awani Mishra <Awani Mishra>
2016-09-04 23:42:58 +0530

    2nd commit

--- app/src/main/java/com/example/telebook/MainActivity.java
+++ app/src/main/java/com/example/telebook/MainActivity.java
@@ -1,23 +1,31 @@
 package com.example.telebook;
 
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1;
+
     @Override
-    protected    void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        b1 = (Button) findViewById(R.id.button_log_in);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        Button b1;
 
-            }
-        });
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager_home);
+        viewPager.setAdapter(new CustomPagerAdapter(this));
+            b1 = (Button) findViewById(R.id.Login_home_page);
+            b1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i_login = new Intent(MainActivity.this,Login.class);
+                    startActivity(i_login);
+                }
+            });
 
+        }
     }
-}
+
