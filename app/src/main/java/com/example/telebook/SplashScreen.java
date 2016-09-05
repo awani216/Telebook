473e69d13129207b3219b0e26037052c4fd0a8e8
iket0512 <iket.ag24@gmail.com>
2016-09-05 10:33:13 +0530

    changes made in splash screen..added animation text

--- app/src/main/java/com/example/telebook/SplashScreen.java
+++ app/src/main/java/com/example/telebook/SplashScreen.java
@@ -5,23 +5,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import me.wangyuwei.particleview.ParticleView;
+
 /**
  * Created by Awani on 8/30/2016.
  */
 public class SplashScreen extends Activity{
-
+    private ParticleView anime_text;
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 4000;
+    private static int SPLASH_TIME_OUT = 8000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.screen_splash);
+        anime_text=(ParticleView)findViewById(R.id.anim_text);
+        anime_text.startAnim();
+
+        anime_text.setOnParticleAnimListener(new ParticleView.ParticleAnimListener() {
+            @Override
+            public void onAnimationEnd() {
+
+            }
+        });
+
         new Handler().postDelayed(new Runnable() {
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
 
             @Override
             public void run() {
